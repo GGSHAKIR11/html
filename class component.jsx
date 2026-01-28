db08f3cf2bb75c2b609e08c22488b8c49c6bfb2a
GGSHAKIR11 <mulumbashakir@gmail.com>
2026-01-27 19:33:19 -0500

    Add files via upload

---
+++ class component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class student extends React.Component {
+    render() {
+        return <h2>Hi, i am codiganl student</h2>;
+}
+}
+
+ReactDom.render(<student />, document.getElementById('root'));import React from 'react';
+import ReactDOM from 'react-dom';
+
+class student extends React.Component {
+    render(){
+        return <h2>Hi, i am codiganl student</h2>;
+    }
+}
