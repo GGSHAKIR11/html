5662f93772df79273ad0206e6c7b81a4029d6c50
GGSHAKIR11 <mulumbashakir@gmail.com>
2026-01-27 19:40:23 -0500

    Add files via upload

---
+++ Function component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function student(){
+    return <h2>Hi, i am codiganl student</h2>
+}
+
+ReactDOM.render(<student />,document.getElementById('root'))
